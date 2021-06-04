31cef8b8d55979203f6f5c85d504e0f1825f49fd
mingg123 <s_p_g_b_@nate.com>
2021-06-04 15:46:02 +0900

    feat : add example 1

---
+++ index.js
@@ -0,0 +1,58 @@
+import fs from 'fs';
+const playsStr = fs.readFileSync('./data/plays.json', 'utf-8');
+const invoicesSte = fs.readFileSync('./data/invoices.json', 'utf-8');
+
+const plays = JSON.parse(playsStr);
+const invoices = JSON.parse(invoicesSte);
+
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역(고객명: ${invoice.customer})\n`;
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format;
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+
+    switch (
+      play.type //비극
+    ) {
+      case 'tragedy':
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case 'comedy': //희극
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    //포인트를 적립한다
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 희극 관객이 5명마다 추가 포인트를 제공한다.
+    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    //청구 내역을 출력한다.
+    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    } seats)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits} 점\n`;
+  return result;
+}
+
+const res = statement(invoices[0], plays);
+console.log(res);
