d47b60f86d10bd2268370f670d619a0fe002ef4c
mingg123 <s_p_g_b_@nate.com>
2021-06-06 16:40:30 +0900

    refactor : chaper4 테스트 코드 예제

--- ch4/example.js
+++ ch4/example.js
@@ -1,72 +1,72 @@
-class Province {
-  constructor(doc) {
-    this._name = doc._name;
-    this._producers = [];
-    this._totalProduction = 0;
-    this._demand = doc.demand;
-    this._price = doc.price;
-    doc.producers.forEach((d) => this.addProducer(new Producer(this, d)));
-  }
+// class Province {
+//   constructor(doc) {
+//     this._name = doc._name;
+//     this._producers = [];
+//     this._totalProduction = 0;
+//     this._demand = doc.demand;
+//     this._price = doc.price;
+//     doc.producers.forEach((d) => this.addProducer(new Producer(this, d)));
+//   }
 
-  get name() {
-    return this._name;
-  }
-  get producers() {
-    return this._producers;
-  }
-  get totalProduction() {
-    return this._totalProduction;
-  }
-  set totalProduction(arg) {
-    this._totalProduction = arg;
-  }
-  get demand() {
-    return this._demand;
-  }
-  set demand(arg) {
-    this._demand = parseInt(arg);
-  }
-  get price() {
-    return this._price;
-  }
-  set price(arg) {
-    this._price = parseInt(arg);
-  }
+//   get name() {
+//     return this._name;
+//   }
+//   get producers() {
+//     return this._producers;
+//   }
+//   get totalProduction() {
+//     return this._totalProduction;
+//   }
+//   set totalProduction(arg) {
+//     this._totalProduction = arg;
+//   }
+//   get demand() {
+//     return this._demand;
+//   }
+//   set demand(arg) {
+//     this._demand = parseInt(arg);
+//   }
+//   get price() {
+//     return this._price;
+//   }
+//   set price(arg) {
+//     this._price = parseInt(arg);
+//   }
 
-  addProducer(arg) {
-    this._producers.push(arg);
-    this._totalProduction += arg.production;
-  }
+//   addProducer(arg) {
+//     this._producers.push(arg);
+//     this._totalProduction += arg.production;
+//   }
 
-  get shortfall() {
-    return this._demand - this.totalProduction;
-  }
+//   get shortfall() {
+//     return this._demand - this.totalProduction;
+//   }
 
-  get profit() {
-    return this.demandValue - this.demandCost;
-  }
+//   get profit() {
+//     return this.demandValue - this.demandCost;
+//   }
 
-  get demandValue() {
-    return this.satisfiedDemand * this.price;
-  }
+//   get demandValue() {
+//     return this.satisfiedDemand * this.price;
+//   }
 
-  get satisfiedDemand() {
-    return Math.min(this._demand, this.totalProduction);
-  }
+//   get satisfiedDemand() {
+//     return Math.min(this._demand, this.totalProduction);
+//   }
 
-  get demandCost() {
-    let remainingDemand = this.demand;
-    let result = 0;
-    this.producers
-      .sort((a, b) => a.cost - b.cost)
-      .forEach((p) => {
-        const contribution = Math.min(remainingDemand, p.production);
-        remainingDemand -= contribution;
-        result += contribution * p.cost;
-      });
-    return result;
-  }
-}
+//   get demandCost() {
+//     let remainingDemand = this.demand;
+//     let result = 0;
+//     this.producers
+//       .sort((a, b) => a.cost - b.cost)
+//       .forEach((p) => {
+//         const contribution = Math.min(remainingDemand, p.production);
+//         remainingDemand -= contribution;
+//         result += contribution * p.cost;
+//       });
+//     return result;
+//   }
+// }
 
 class Producer {
   constructor(aProvince, data) {
